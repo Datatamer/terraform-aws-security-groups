6a05d9f978c2c7f1ae1bba9994d83bb0d7dfc738
Dan Souza <dan.souza@tamr.com>
2022-08-04 12:43:04 -0400

    formatting updates to conform to linting

--- examples/minimal/variables.tf
+++ examples/minimal/variables.tf
@@ -15,8 +15,8 @@ variable "egress_cidr_blocks" {
 variable "tags" {
   type        = map(string)
   description = "A map of tags to add to all resources created by this example."
-  default     = { 
-    Author = "Tamr"
+  default = {
+    Author      = "Tamr"
     Environment = "Example"
   }
 }
